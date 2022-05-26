2a0ef11312c22ecee5654253f66858f5378ef7d2
Arafat <masona954@gmail.com>
2022-05-26 11:00:44 +0600

    index updated

--- server/index.js
+++ server/index.js
@@ -10,9 +10,13 @@ app.get("/api", (req, res) => {
 });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-}); 
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static('client/build'));
+
+  app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"client","build", "index.html"))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
