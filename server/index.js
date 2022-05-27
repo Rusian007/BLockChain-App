343128ed018f821990d8163aa0c47d4e8d8cdef5
Arafat <masona954@gmail.com>
2022-05-27 13:15:58 +0600

    Database Connected

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 
 const express = require("express");
 
+const { MongoClient } = require("mongodb");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+const uri = "mongodb+srv://rusian:hellsheart@survaycluster.nowszxv.mongodb.net/?retryWrites=true&w=majority";
+MongoClient.connect(uri,{useNewUrlParser:true}, async function(err, client){
+  const db = client.db('survay')
+  console.log("DB COnnected")
+
+  const dbstuffs = db.collection("creation")
+
+  //await dbstuffs.insertOne({data: "FOX"})
+  console.log("Done!")
+  //const results  = await db.collection("creation").find().toArray()
+  //console.log(results)
+})
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
