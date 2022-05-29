4452ac2d24ed6dee424146194bfd529bc8e856e9
Arafat <masona954@gmail.com>
2022-05-29 18:02:03 +0600

    Updated Abouts

--- client/src/App.js
+++ client/src/App.js
@@ -8,15 +8,19 @@ function App() {
 
   const [data, setData] = React.useState(null);
   React.useEffect(() => {
-    fetch("/api")
+    fetch("/api/home")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) =>{
+       // console.log(data)
+       setData(data.message)
+     });
   }, []);
 
   return (
     <div className="App">
     <nav>
     <Link to="/about">About us</Link>
+    <h2> {data} </h2>
     </nav>
     </div>
   );
--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,13 +1,14 @@
 import React,{Fragment} from 'react';
+import '../App.css';
 
 const About = () =>{
     return (
-    <Fragment>
+    <div className="App">
         <h1>Some RDS App</h1>
         <h2>An online platform for নসূ</h2>
         
         <p> This app is under development</p>
-    </Fragment>
+    </div>
     )
 }
 
