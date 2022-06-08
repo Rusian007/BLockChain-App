cab52fb4aee429d7378b544500c1425c1b9497a9
Arafat Rahman <masona954@gmail.com>
2022-06-08 21:00:07 +0600

    second

--- nexts/.next/static/chunks/webpack.js
+++ nexts/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7628353225ac04d0"; }
+/******/ 		__webpack_require__.h = function() { return "001f3d03ba271200"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
