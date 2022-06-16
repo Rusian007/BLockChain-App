d341243160c79de7d1eb610d69f1e08c3c5c9561
Arafat <masona954@gmail.com>
2022-06-16 20:37:00 +0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import getWeb3 from "./getWeb3";
+import "./App.css";
+
+const App = () =>{
+  return(
+    <>
+    <div className="App">
+      React is working 
+      
+    </div>
+    <div className="App"> Add Styles and stuff</div>
+    </>
+    )
+}
+
+export default App;
