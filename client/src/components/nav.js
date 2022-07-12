d607537e1051fcca95b46de0bac871f2bd7e0438
Arafat <masona954@gmail.com>
2022-07-12 07:16:12 +0600

    home added

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -1,39 +1,70 @@
-import React from 'react';
-import styles from '../Css/nav.module.css';
+import React, { useState } from "react";
+import styles from "../Css/nav.module.css";
+import { ImCross } from "react-icons/im";
+import { CgOptions } from "react-icons/cg";
 
-export default function TopNav (){
-	return(
-		<>
-			<nav className={`${styles.flex} ${styles.nav}`}>
+export default function TopNav() {
+  const [toggle, setToggle] = useState(false);
 
-				<div >
-					<img className={styles.Logo} src="https://cdn.tutsplus.com/gamedev/uploads/legacy/043_freeShmupSprites/Free_Shmup_Sprites_Boss_Battle.jpg"  alt="App Logo"/>
-				</div>
+  const ToggleMenu = e => {
+	  if (toggle){
+		setToggle(false)
+	  } else {
+		setToggle(true)
+	  }
+  };
 
-				<div className={`${styles.btn_group} ${styles.flex}`}>
+  return (
+    <>
+        <div className={`${styles.menu} ${toggle? styles.in_animate : ""}`}>
+          <div style={{ textAlign: "end" }}>
+            <button onClick={ToggleMenu} className={styles.btn_toggle}>
+              <ImCross style={{ color: "white" }} />
+            </button>
+          </div>
 
-					<div>
-						<button className={styles.bg_none}>
-							Upload Files
-						</button>
-					</div>
+          <div>
+            <button className={styles.bg_none}>Upload Files</button>
+          </div>
 
-					<div>
-						<button className={styles.btn}>
-							Share Files
-						</button>
-					</div>
+          <div>
+            <button className={styles.btn}>Share Files</button>
+          </div>
 
-					<div>
-						<button className={styles.btn}>
-							Profile
-						</button>
-					</div>
+          <div>
+            <button className={styles.btn}>Profile</button>
+          </div>
+        </div>
 
-				</div>
+      <nav className={`${styles.flex} ${styles.nav}`}>
+        <div>
+          <img
+            className={styles.Logo}
+            src="https://cdn.tutsplus.com/gamedev/uploads/legacy/043_freeShmupSprites/Free_Shmup_Sprites_Boss_Battle.jpg"
+            alt="App Logo"
+          />
+        </div>
 
-			</nav>
-		</>
-		)
+        <div className={`${styles.btn_group} ${styles.flex}`}>
+          <div>
+            <button className={styles.bg_none}>Upload Files</button>
+          </div>
 
-};
+          <div>
+            <button className={styles.btn}>Share Files</button>
+          </div>
+
+          <div>
+            <button className={styles.btn}>Profile</button>
+          </div>
+        </div>
+
+        <div className={styles.hidden}>
+          <button onClick={ToggleMenu} className={styles.btn_toggle}>
+            <CgOptions style={{ fontSize: "29px", color: "white" }} />
+          </button>
+        </div>
+      </nav>
+    </>
+  );
+}
--- client/src/routes/home.js
+++ client/src/routes/home.js
@@ -6,28 +6,17 @@ import styles from '../Css/home.module.css';
 const Home = () => {
   return (    
   <section className={styles.outerdiv}>
+
       <TopNav />
-        <div className="container-sm">
-          <div className="innerdiv">
-            <div className="hero-content">
-              <h1
-                className="mt-0 mb-16 reveal-from-bottom"
-                data-reveal-delay="200"
-              >
-                OMG NOOOO
-              </h1>
-              <div className="container-xs">
-                <p
-                  className="m-0 mb-32 reveal-from-bottom"
-                  data-reveal-delay="400"
-                >
-                  Some text need to be added
-                </p>
-              </div>
-            </div>
-          </div>
+
+        <div className={styles.main_container}>
+          <p>
+            Add Files here
+          </p>
         </div>
-        <Footer />
+
+      <Footer />
+
       </section>
 
   );
