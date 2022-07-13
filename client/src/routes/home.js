bc1e865247f4c8d9914b8308b4a4886c1cdbbb3c
Arafat <masona954@gmail.com>
2022-07-13 18:31:24 +0600

    Cards added

--- client/src/routes/home.js
+++ client/src/routes/home.js
@@ -1,75 +1,75 @@
-import React, { useState,useRef,useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TopNav from "../components/nav";
 import Footer from "../components/footer";
-import styles from '../Css/home.module.css';
+import styles from "../Css/home.module.css";
 import { useNavigate } from "react-router-dom";
 import userContract from "../contracts/users.json";
-import Web3 from 'web3';
-import { create } from 'ipfs-http-client';
+import Web3 from "web3";
+import { create } from "ipfs-http-client";
 import { toast, ToastContainer } from "react-toastify";
+import { FiDownload } from "react-icons/fi";
+import { FiLink } from "react-icons/fi";
 
 const Home = () => {
-  let account = sessionStorage.getItem("accounts")
-  const client = create('https://ipfs.infura.io:5001/api/v0');
+  let account = sessionStorage.getItem("accounts");
+  const client = create("https://ipfs.infura.io:5001/api/v0");
   const [file, setFile] = useState(null);
-  const [urls , setUrls] = useState(null);
+  const [urls, setUrls] = useState(null);
   let btnref = useRef(null);
-  let web3 = null,networkId = sessionStorage.getItem("networkId"),instance=null
+  let web3 = null,
+    networkId = sessionStorage.getItem("networkId"),
+    instance = null;
 
-  if(account){
-  web3 = new Web3(Web3.givenProvider || "http://localhost:7545")
-  const deployedNetwork = userContract.networks[networkId];
-  instance = new web3.eth.Contract(
-            userContract.abi,
-            deployedNetwork && deployedNetwork.address, //if there is a deployed network then get the address
-          );
+  if (account) {
+    web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+    const deployedNetwork = userContract.networks[networkId];
+    instance = new web3.eth.Contract(
+      userContract.abi,
+      deployedNetwork && deployedNetwork.address //if there is a deployed network then get the address
+    );
   }
 
   useEffect(() => {
     // Update the document title using the browser API
     document.title = `HOME | Cloud`;
-    ReturnHash()
+    ReturnHash();
   }, [1]);
 
-  const GetFIles = (e) => {
-
+  const GetFIles = e => {
     const reader = new FileReader();
-    let thefile = reader.readAsArrayBuffer(e.target.files[0]);   
+    let thefile = reader.readAsArrayBuffer(e.target.files[0]);
 
     reader.onloadend = () => {
-      thefile = Buffer(reader.result)
-      setFile(thefile)
-      btnref.current.click()
-    }
+      thefile = Buffer(reader.result);
+      setFile(thefile);
+      btnref.current.click();
+    };
 
-    showSuccess("File Uploaded for Processing 😄" )
-    
-    return
-    
+    showSuccess("File Uploaded for Processing 😄");
+
+    return;
+
     //
-    
   };
-  const UploadFile = async()=>{
-    
-    const added = await client.add(file)
-    
+  const UploadFile = async () => {
+    const added = await client.add(file);
 
     // added.path has the string, Store the string here
     // ############ //
     // https://ipfs.io/ipfs/<CID>
 
-    
-    await instance.methods.HashStore(account, added.path).send({ from: account })
-    ReturnHash()
-    
-  }
+    await instance.methods
+      .HashStore(account, added.path)
+      .send({ from: account });
+    ReturnHash();
+  };
 
-  const ReturnHash =async()=>{
-    const res = await instance.methods.HashSetReturn(account).call()
-    setUrls(res)
-  }
+  const ReturnHash = async () => {
+    const res = await instance.methods.HashSetReturn(account).call();
+    setUrls(res);
+  };
 
-  const showSuccess = (msg) => {
+  const showSuccess = msg => {
     toast.success(msg, {
       position: "top-center",
       autoClose: 3000,
@@ -81,49 +81,69 @@ const Home = () => {
     });
   };
 
-  if(account){
-    return (    
-  <section className={styles.outerdiv}>
-  <button className={styles.hide} ref={btnref} onClick={UploadFile} >Click TO State</button>
-  <button onClick={ReturnHash}> me ! </button>
-  <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
+  if (account) {
+    return (
+      <section className={styles.outerdiv}>
+        <button className={styles.hide} ref={btnref} onClick={UploadFile}>
+          Click TO State
+        </button>
 
-      <TopNav getTheFile={GetFIles} />
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+
+        <TopNav getTheFile={GetFIles} />
 
         <div className={styles.main_container}>
+          {urls
+            ? urls.map((element, index) => {
+                return (
+                  <div className={styles.card} key={index}>
+                    <div className={styles.card_container}>
 
-          {urls ? urls.map((element, index)=> {
-              return (
-                <div key={index}> box </div>
-                )
-            }
-          ):""}
+                      <div className={styles.head}>
+
+                        <h2>File Name Here</h2>
+
+                        <div className={styles.subType}>
+                          <span> File Type </span>
+                        </div>
+
+                      </div>
+
+                      <div className={styles.btn_div}>
+                        <button className={styles.bg_blue}><FiDownload /></button>
+                        <button className={styles.bg_red}><FiLink /></button>
+                      </div>
+
+                    </div>
+                  </div>
+                );
+              })
+            : ""}
         </div>
 
-      <Footer />
-
+        <Footer />
       </section>
-
-  )
+    );
   } else {
     return (
       <>
-      <h2 style={{ textAlign : 'center', padding :'40px' }}> You Need To Log In First </h2>
+        <h2 style={{ textAlign: "center", padding: "40px" }}>
+          {" "}
+          You Need To Log In First{" "}
+        </h2>
       </>
-    )
+    );
   }
-  
-  
 };
 
 export default Home;
